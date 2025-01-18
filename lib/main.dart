608eb4145466b5cb1e4e109f314a90d610052161
GreatMov <alexandru.movsessian@gmail.com>
2025-01-19 00:11:48 +0200

    1'st commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const Zar());
+}
+
+class Zar extends StatefulWidget {
+  const Zar({super.key});
+
+  @override
+  State<Zar> createState() => _ZarState();
+}
+
+class _ZarState extends State<Zar> {
+
+  int zarstanga = 1;
+  int zardreapta = 1;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextButton(
+                  onPressed: (){
+                    setState(() {
+                      zarstanga = Random().nextInt(6)+1;
+                      zardreapta = Random().nextInt(6)+1;
+                    });
+                  },
+                  child: Image(
+                    image: ExactAssetImage('images/$zarstanga.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: (){
+                    setState(() {
+                      zardreapta = Random().nextInt(6)+1;
+                    });
+                  },
+                  child: Image(
+                    image: ExactAssetImage('images/$zardreapta.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
